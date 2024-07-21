ca77bceafcc1902d7342ebe7f5a457e6f29efba7
azimjonshodiyorov <azimjonshodiyorov@gmail.com>
2024-07-21 16:19:03 +0500

    2 day

---
+++ LeetCodeSolution/ReverseWordsStringIII557.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeSolution;
+
+public class ReverseWordsStringIii557
+{
+    public string ReverseWords(string s)
+    {
+        string answer = "";
+        string[] array = s.Split(new Char[] { ' ' });
+
+        foreach (var item in array)
+        {
+            for (int i = item.Length; i>0; i--)
+            {
+                answer += item[i];
+            }
+            answer += " ";
+        }
+
+        return answer.Remove(answer.Length-1);
+    }
+}
